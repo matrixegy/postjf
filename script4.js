72d75d8279df7984ba61295fc14a46347eaadb24
matrixegy <mustaphamahmoud1989@gmail.com>
2025-01-24 07:13:14 +0200

    Update script4.js

--- script4.js
+++ script4.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
 
             iframe.width = '100%';
-            iframe.height = '360';
+            iframe.height = '405';
             iframe.allow = 'autoplay; encrypted-media';
             iframe.frameBorder = '0';
 
