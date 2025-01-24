bbc2bde7d370815b6c9f883e0ac0b1c4b331cf55
matrixegy <mustaphamahmoud1989@gmail.com>
2025-01-24 07:40:46 +0200

    Update script4.js

--- script4.js
+++ script4.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
 
             iframe.width = '100%';
-            iframe.height = '360';
+            iframe.height = '405';
             iframe.allow = 'autoplay; encrypted-media';
             iframe.frameBorder = '0';
 
